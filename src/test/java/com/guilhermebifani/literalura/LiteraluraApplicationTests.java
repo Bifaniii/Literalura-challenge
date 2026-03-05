d3ce2dc67714a6295c7b88277918583a4db8726a
Bifaniii <gbifani.tech@gmail.com>
2026-03-05 14:41:11 -0300

    Commit inicial: estrutura Spring Boot

---
+++ src/test/java/com/guilhermebifani/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermebifani.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
